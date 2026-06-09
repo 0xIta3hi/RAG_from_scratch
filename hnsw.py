9d8dd479a2e40122ae0e9c23032ca3c6d544f385
0xIta3hi <aryanbharadiyak2006@gmail.com>
2026-06-09 20:25:13 +0530

    Implemented Class HNSW_Node

--- hnsw.py
+++ hnsw.py
@@ -8,3 +8,12 @@ def cosine_similarity(v1:list[float], v2:list[float]) -> list[float]:
     cos_sim = dot_product / (v1_mag * v2_mag)
     return cos_sim
 
+class HNSW_Node:
+    def __init__(self, node_id:int, vector:list[float], metadata:list, max_level:int):
+        self.node_id = node_id
+        self.vector = vector
+        self.metadata = metadata
+        # maps layer number -> list of neighbor node IDs  
+        self.connections = {layer:[] for layer in range(max_level + 1)}
+
+        
